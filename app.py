760108fcba7726840cc5e8ac56065dba6057f98c
layan18-meet <mlayan147@gmail.com>
2018-02-02 23:38:35 +0200

    sign in not working..

--- app.py
+++ app.py
@@ -23,9 +23,17 @@ db.create_all()
 def index():
 	return render_template("index.html")
 
+@app.route('/home')
+def index2():
+	return render_template("index2.html")
+
 @app.route('/about')
 def about():
 	return render_template("about.html")
+
+@app.route('/About')
+def about_before():
+	return render_template("about_before.html")
 
 @app.route('/articles')
 def articles():
@@ -55,11 +63,11 @@ def alternatives():
 def debate():
 	return render_template("debate.html")
 
-@app.route('/signIn')
+@app.route('/sign_in')
 def sign_in():
 	return render_template("sign_in.html")
 
-@app.route('/signUp')
+@app.route('/sign_up')
 def sign_up():
 	return render_template("sign_up.html")
 
@@ -76,44 +84,29 @@ def signUp():
 	else:
 		return render_template("sign_up.html")
 
+@app.route('/signIn', methods=['GET', 'POST'])
+def signIn():
+	if request.method == "GET":
+		return render_template('sign_in.html')
+	elif request.method == "POST":
+		log_in = User.query.filter_by(user=request.form['name'], password=request.form['password']).first()
+	if log_in == None:
+		return render_template('sign_in.html', log_in=False)
+	else:
+		return render_template('index2.html')
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect('index.html')
+
+
+
+
+
 if __name__== '__main__':
 	app.secret_key = 'super secret key'
 	app.config['SESSION_TYPE'] 	= 'filesystem'
 	# sess.init_app(app)
 	app.run(debug=True)
-
-
-@app.route('/signIn', methods=['GET', 'POST'])
-def signIn():
-    # Here we use a class of some kind to represent and validate our
-    # client-side form data. For example, WTForms is a library that will
-    # handle this for us, and we use a custom LoginForm to validate.
-    form = LoginForm()
-    if form.validate_on_submit():
-        # Login and validate the user.
-        # user should be an instance of your `User` class
-        login_user(user)
-
-        flask.flash('Logged in successfully.')
-
-        next = flask.request.args.get('next')
-        # is_safe_url should check if the url is safe for redirects.
-        # See http://flask.pocoo.org/snippets/62/ for an example.
-        if not is_safe_url(next):
-            return flask.abort(400)
-
-        return redirect(next or url_for('index'))
-    return render_template('sign_in.html', form=form)
-
-    @app.route("/logout")
-    @login_required
-    def logout():
-        logout_user()
-        return redirect('index.html')
-
-#!/usr/bin/python
-#flask-login anonymous user class
-from flask.ext.login import AnonymousUserMixin
-class Anonymous(AnonymousUserMixin):
-  def __init__(self):
-    self.username = 'Guest'
